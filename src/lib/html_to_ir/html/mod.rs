4502b9927d41764e27bd60452d47a6740d333578
Grey Hill <infogreytech@gmail.com>
2017-04-28 20:27:20 -0700

    Refactor function to module

--- src/lib/html_to_ir/html/mod.rs
+++ src/lib/html_to_ir/html/mod.rs
@@ -15,38 +15,6 @@ pub use self::nodes::Nodes;
 mod document;
 pub use self::document::Document;
 
-fn convert(handle: &Handle) -> Node {
-    let node = handle.borrow();
-
-    use self::html5ever::rcdom::NodeEnum::*;
-    match node.node {
-        Element(ref name, _, ref attrs) => {
-            let tag = name.local.to_string();
-
-            let mut attributes = Vec::new();
-            for attr in attrs {
-                let key = attr.name.local.to_string();
-                let value = attr.value.as_ref().to_string();
-
-                attributes.push((key, value));
-            }
-
-            let children = node.children
-                .iter()
-                .map(|child| convert(child))
-                .collect();
-
-            Node::Element {
-                tag: tag,
-                attributes: attributes,
-                children: children,
-            }
-        }
-        Text(ref text) => Node::Text(text.as_ref().to_string()),
-        _ => Node::Text("".to_string()),
-    }
-}
-
 pub fn convert_dom(handle: &Handle) -> Result<Document, &'static str> {
     let node = handle.borrow();
 
@@ -55,7 +23,7 @@ pub fn convert_dom(handle: &Handle) -> Result<Document, &'static str> {
         NodeEnum::Document => {
             Ok(node.children
                 .iter()
-                .map(|child| convert(child))
+                .map(Node::from)
                 .fold(Document::new(), |doc, child| doc.add(child)))
         }
         _ => Err("Converting from non-Document node"),
--- src/lib/html_to_ir/html/node.rs
+++ src/lib/html_to_ir/html/node.rs
@@ -1,3 +1,7 @@
+extern crate html5ever;
+
+use self::html5ever::rcdom::Handle;
+
 use super::Nodes;
 
 #[derive(Debug)]
@@ -8,4 +12,40 @@ pub enum Node {
         attributes: Vec<(String, String)>,
         children: Nodes,
     },
+}
+
+impl <'a> From<&'a Handle> for Node {
+    fn from(handle: &Handle) -> Self {
+        let node = handle.borrow();
+
+        use self::html5ever::rcdom::NodeEnum;
+        match node.node {
+            NodeEnum::Element(ref name, _, ref attrs) => {
+                let tag = name.local.to_string();
+
+                let mut attributes = Vec::new();
+                for attr in attrs {
+                    let key = attr.name.local.to_string();
+                    let value = attr.value.as_ref().to_string();
+
+                    attributes.push((key, value));
+                }
+
+                let children = node.children
+                    .iter()
+                    .map(|child| Node::from(child))
+                    .collect();
+
+                Node::Element {
+                    tag,
+                    attributes,
+                    children
+                }
+            }
+            NodeEnum::Text(ref text) => {
+                Node::Text(text.as_ref().to_string())
+            }
+            _ => Node::Text(String::new()),
+        }
+    }
 }
